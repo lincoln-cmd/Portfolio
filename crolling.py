d040a0ac1b2538f3073c0448f1cebce652ba92ab
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-02-16 21:45:16 +0900

    Add files via upload

--- crolling.py
+++ crolling.py
@@ -8,9 +8,39 @@ radiuslist = []
 
 res = requests.get('https://en.wikipedia.org/wiki/List_of_Solar_System_objects_by_size')
 soup = BeautifulSoup(res.content, 'html.parser')
-data = soup.select('div#content.mw-body > div#bodyContent.mw-body-content > div#mw-content-text.mw-content-ltr > div.mw-parser-output > table.wikitable sortable jquery-tablesorter > tbody > tr > td > a')
 
-for i in data:
-    namelist.append(i)
     
+#print(masslist)
+
+n = range(3, 38)
+for i in n:
+    data = soup.select('#mw-content-text > div.mw-parser-output > table:nth-child(21) > tbody > tr:nth-child({0}) > td:nth-child(1) > a'.format(i))
+    data2 = soup.select('#mw-content-text > div.mw-parser-output > table:nth-child(21) > tbody > tr:nth-child({0}) > td:nth-child(3) > span'.format(i))
+    data3 = soup.select('#mw-content-text > div.mw-parser-output > table:nth-child(21) > tbody > tr:nth-child({0}) > td:nth-child(7) > span'.format(i))
+    for j in data:
+        namelist.append(j.get_text())
+    for j in data2:
+        radiuslist.append(j.get_text())
+    for j in data3:
+        masslist.append(j.get_text())
+
+for i in range(len(masslist)):
+    if '±' in masslist[i]:
+        n = masslist[i].index('±')
+        new = masslist[i][:n]
+        masslist[i] = new
+
+for i in range(len(radiuslist)):
+    if '±' in radiuslist[i]:
+        n2 = radiuslist[i].index('±')
+        new = radiuslist[i][:n2]
+        radiuslist[i] = new
+    
+masslist.insert(31, 0)
+del radiuslist[3]
+masslist[23] = 3.1
+radiuslist[23] = 715
+
 print(namelist)
+print(masslist)
+print(radiuslist)
