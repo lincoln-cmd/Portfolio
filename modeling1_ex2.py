374fb048aabad45aabdba00f0109e9a3af873de8
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-04-09 21:47:23 +0900

    Astrophysics

--- modeling1_ex2.py
+++ modeling1_ex2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting
 from astroquery.vizier import Vizier
 import scipy.optimize
-%matplotlib inline
+#%matplotlib inline
 
 '''
  - make the fake data in the shape of a Gaussian Distribution
@@ -14,8 +14,9 @@ import scipy.optimize
  - about Gaussian Distribution : https://docs.scipy.org/doc/scipy-1.0.0/reference/generated/scipy.optimize.curve_fit.html
 '''
 mu, sigma, amplitude = 0.0, 10.0, 10.0
+N = 100
 N2 = 100
-x2 = np.linspace(-30, 30, N)
+x2 = np.linspace(-30, 30, N2)
 y2 = amplitude * np.exp(-(x2 - mu)**2 / (2*sigma**2))
 y2 = np.array([y_point + np.random.normal(0, 1) for y_point in y2])
 sigma = 1
@@ -44,5 +45,39 @@ print('Mean : {} +\- {}'.format(best_fit_gauss.mean.value, np.sqrt(cov_diag[1]))
 print('Standard Deviation : {} +\- {}'.format(best_fit_gauss.stddev.value, np.sqrt(cov_diag[2])))
 
 # create the comparable thing by employing the 'scipy.optimize.curve_fit'
+def f(x, a, b, c):
+    return a * np.exp(-(x - b)**2 / (2.0 * c**2))
+p_opt, p_cov = scipy.optimize.curve_fit(f, x2, y2, sigma = y1_err)
+a, b, c = p_opt
+best_fit_gauss_2 = f(x2, a, b, c)
+print(p_opt)
 
+print('Amplitude : {} +\- {}'.format(p_opt[0], np.sqrt(p_cov[0, 0])))
+print('Mean : {} +\- {}'.format(p_opt[1], np.sqrt(p_cov[1, 1])))
+print('Standard Deviation : {} +\- {}'.format(p_opt[2], np.sqrt(p_cov[2, 2])))
+
+def calc_reduced_chi_square(fit, x, y, yerr, N, n_free):
+    '''
+    fit(array) : values for the fit
+    x, y, yerr(array) : data
+    N : Total number of points
+    n_free : number of parameters we are fitting
+    '''
+    return 1.0 / (N - n_free) * sum(((fit - y) / yerr)**2)
+reduced_chi_squared = calc_reduced_chi_square(best_fit_gauss(x2), x2, y2, y2_err, N2, 3)
+print('Reduced Chi Squared using astropy.modeling : {}'.format(reduced_chi_squared))
+
+reduced_chi_squared = calc_reduced_chi_square(best_fit_gauss_2, x2, y2, y2_err, N2, 3)
+print('Reduced Chi Squared using scipy : {}'.format(reduced_chi_squared))
+'''
+ Above values have small difference in the Reduced Chi Squared. While the astropy.modeling needs to change the name of the fitter
+and the model to perform a completely different fit, scipy requires to remember the expression of the function
+'''
+
+plt.errorbar(x2, y2, yerr = y2_err, fmt='k.')
+plt.plot(x2, best_fit_gauss(x2), 'g-', linewidth = 6, label = 'astropy.modeling')
+plt.plot(x2, best_fit_gauss_2, 'r-', linewidth = 2, label = 'scipy')
+plt.xlabel('$x_2$')
+plt.ylabel('$y_2$')
+plt.legend()
 
---
+++ modeling1_ex3.py
@@ -0,0 +1,68 @@
+# modeling1_ex3
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.modeling import models, fitting
+from astroquery.vizier import Vizier
+import scipy.optimize
+
+'''
+ 1. model
+ 2. fitter
+ 3. perform fit to data
+'''
+
+
+
+'''
+ - about expectation, variance, standard deviation, and covariance Matrix : http://matrix.skku.ac.kr/math4ai-intro/W11/
+     https://blogs.ubc.ca/math105/continuous-random-variables/expected-value-variance-standard-deviation/
+ - about 
+'''
+# model : Sine1D
+# fitter : 
+
+N3 = 100
+x3 = np.linspace(0, 3, N3)
+y3 = 5.0 * np.sin(2 * np.pi * x3)
+y3 = np.array([y_point + np.random.normal(0, 1) for y_point in y3])
+sigma = 1.5
+y3_err = np.ones(N) * sigma
+
+
+plt.errorbar(x3, y3, yerr = y3_err, fmt = 'k.')
+plt.xlabel('$x_3$')
+plt.ylabel('$y_3$')
+
+
+model_sine = models.Sine1D()
+fitter_sine = fitting.LevMarLSQFitter()
+best_fit_sine = fitter_sine(model_sine, x3, y3, weights = 1.0 / y3_err**2)
+print(best_fit_sine)
+
+print(model_sine.param_names)
+cov_diag = np.diag(fitter_sine.fit_info['param_cov'])
+print(cov_diag)
+
+print('Amplitude : {} +\- {}'.format(best_fit_sine.amplitude.value, np.sqrt(cov_diag[0])))
+print('Frequency : {} +\- {}'.format(best_fit_sine.frequency.value, np.sqrt(cov_diag[1])))
+print('Phase : {} +\- {}'.format(best_fit_sine.phase.value, np.sqrt(cov_diag[2])))
+
+def calc_reduced_chi_square(fit, x, y, yerr, N, n_free):
+    return 1.0 / (N - n_free) * sum(((fit - y)/ yerr)**2)
+
+reduced_chi_squared = calc_reduced_chi_square(best_fit_sine(x3), x3, y3, y3_err, N3, 3)
+print('Reduced Chi Squared with LevMarLSQFitter : {}'.format(reduced_chi_squared))
+
+fitter_sine_2 = fitting.SimplexLSQFitter()
+best_fit_sine_2 = fitter_sine_2(model_sine, x3, y3, weights = 1.0 / y3_err**2)
+
+
+plt.errorbar(x3, y3, yerr = y3_err, fmt = 'k.')
+plt.plot(x3, best_fit_sine(x3), 'r', linewidth = 6, label = 'LevMarLSQFitter')
+plt.plot(x3, best_fit_sine_2(x3), color = 'g', linewidth = 3, label = 'SimplexLSQFitter')
+plt.xlabel(r'$\log_{10}$(Period [days])')
+plt.ylabel('Ks')
+plt.legend()
+
+
