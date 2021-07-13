99a41e83d52ce40b3d0c5fb350d3952fce888bb3
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-07-13 22:00:20 +0900

    Astrophysics

--- UVES_test3.py
+++ UVES_test3.py
@@ -2,10 +2,16 @@ def region_around_line(w, flux, cont):
     indcont = ((w > cont[0][0]) & ( w < cont[0][1])) | ((w > cont[1][0]) & (w < cont[1][1]))
     indrange = (w > cont[0][0]) & (w < cont[1][1])
     f = np.zeros((flux.shape[0], indrange.sum()))
+    print('incont: ', len(indcont))
+    print('indrange : ', len(indrange))
+    #print('f : ', f)
+    print('flux: ', flux)
+    #print('indrage.sum() : ', indrange.sum())
     for i in range(flux.shape[0]):
         linecoeff = np.polyfit(w[indcont], flux[i, indcont], 2)
         f[i,:] = flux[i, indrange] / np.polyval(linecoeff, w[indrange].value)
     return w[indrange], f
 
 wcaII, fcaII = region_around_line(wavelength, flux, [[3925*u.AA, 3930*u.AA], [3938*u.AA, 3945*u.AA]])
-print(wcaII, fcaII)
+#print(wcaII)
+print(fcaII)
