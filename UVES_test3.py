741dd23117a404d94bf0b48224d97dc3139139b0
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-07-14 22:00:37 +0900

    Astrophysics

--- UVES_test3.py
+++ UVES_test3.py
@@ -2,10 +2,10 @@ def region_around_line(w, flux, cont):
     indcont = ((w > cont[0][0]) & ( w < cont[0][1])) | ((w > cont[1][0]) & (w < cont[1][1]))
     indrange = (w > cont[0][0]) & (w < cont[1][1])
     f = np.zeros((flux.shape[0], indrange.sum()))
-    print('incont: ', len(indcont))
-    print('indrange : ', len(indrange))
+    #print('incont: ', indcont)
+    #print('indrange : ', indrange)
     #print('f : ', f)
-    print('flux: ', flux)
+    #print('flux: ', flux)
     #print('indrage.sum() : ', indrange.sum())
     for i in range(flux.shape[0]):
         linecoeff = np.polyfit(w[indcont], flux[i, indcont], 2)
@@ -14,4 +14,10 @@ def region_around_line(w, flux, cont):
 
 wcaII, fcaII = region_around_line(wavelength, flux, [[3925*u.AA, 3930*u.AA], [3938*u.AA, 3945*u.AA]])
 #print(wcaII)
-print(fcaII)
+#print(fcaII)
+
+# Ca2 = wavelength : 393.3682nm, line width : 2.0253nm
+# Ca2(in code) = wavelength : 393.366nm, equivalent width : 20.21238214515653
+ew = fcaII[0,:] - 1.
+ew = ew[:-1] * np.diff(wcaII.to(u.AA).value)
+print(ew.sum())
