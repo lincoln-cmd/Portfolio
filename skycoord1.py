aafc4643517ed6989262c168dd6eadad7dfbf592
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-07-30 21:59:50 +0900

    Astrophysics

--- skycoord1.py
+++ skycoord1.py
@@ -24,7 +24,10 @@ Gaia.ROW_LIMIT = 10000
 '''
  - about SkyCoord : https://docs.astropy.org/en/stable/coordinates/skycoord.html
  - about 'NGC 188' : https://en.wikipedia.org/wiki/NGC_188
-
+ - about 'ICRS' : https://arxiv.org/abs/astro-ph/0602086
+ - astronomical coordinate : https://en.wikipedia.org/wiki/Astronomical_coordinate_systems
+ 
+ 
 
 
 
@@ -34,7 +37,8 @@ Gaia.ROW_LIMIT = 10000
 ngc188_center = SkyCoord(12.11 * u.deg, 85.26*u.deg)
 print(ngc188_center)
 
-
+ngc188_center2 = SkyCoord(12.11 * u.deg, 85.26 * u.deg, frame = 'icrs')
+print(ngc188_center2)
 
 
 
