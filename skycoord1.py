cd77969c965ede96c4385f612c4ff4742a14b439
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-08-02 21:56:59 +0900

    Astrophysics

--- skycoord1.py
+++ skycoord1.py
@@ -27,6 +27,8 @@ Gaia.ROW_LIMIT = 10000
  - about 'ICRS' : https://arxiv.org/abs/astro-ph/0602086
  - astronomical coordinate : https://en.wikipedia.org/wiki/Astronomical_coordinate_systems
  
+ - search astronomical object : http://cdsweb.u-strasbg.fr/cgi-bin/Sesame
+ - astronomical object : https://en.wikipedia.org/wiki/Astronomical_object
  
 
 
@@ -40,5 +42,23 @@ print(ngc188_center)
 ngc188_center2 = SkyCoord(12.11 * u.deg, 85.26 * u.deg, frame = 'icrs')
 print(ngc188_center2)
 
+print(SkyCoord('00h48m26.4s', '85d15m36s', frame = 'icrs'))
 
+print(SkyCoord('00:48:26.4 85:15:36', unit=(u.hour, u.deg), frame = 'icrs'))
+
+ngc188_center = SkyCoord.from_name('NGC 188')
+print(ngc188_center)
+
+#new_center = SkyCoord.from_name('A0620')
+#print('test : ', new_center)
+
+print('ra : ', ngc188_center.ra, 'dec : ', ngc188_center.dec)
+
+print('type of ra : ', type(ngc188_center.ra), 'type of dec : ', type(ngc188_center.dec))
+
+print(ngc188_center.ra.to(u.hourangle), ngc188_center.ra.to(u.radian), ngc188_center.ra.to(u.degree))
+
+print(ngc188_center.ra.hour, ngc188_center.ra.radian, ngc188_center.ra.degree)
+
+print(ngc188_center.ra.to_string(unit = u.hourangle, sep = ':', pad = True))
 
