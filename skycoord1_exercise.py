dabf5211a7062b9f970b6428d81f82c527a5d428
lincoln-cmd <51924537+lincoln-cmd@users.noreply.github.com>
2021-08-06 21:57:32 +0900

    Astrophysics

--- skycoord1_exercise.py
+++ skycoord1_exercise.py
@@ -31,3 +31,40 @@ parallax_snr = ngc188_table['parallax'] / ngc188_table['parallax_error']
 ngc188_table_3d = ngc188_table[parallax_snr > 10]
 print(len(ngc188_table_3d))
 
+print(Distance(parallax = 1 * u.mas))
+
+gaia_dist = Distance(parallax = ngc188_table_3d['parallax'])
+
+ngc188_coords_3d = SkyCoord(ra = ngc188_table_3d['ra'], dec = ngc188_table_3d['dec'], distance = gaia_dist)
+print(ngc188_coords_3d)
+
+
+
+fig, ax = plt.subplots(figsize = (6.5, 5.2), constrained_layout = True)
+cs = ax.scatter(ngc188_coords_3d.ra.degree, ngc188_coords_3d.dec.degree, c = ngc188_coords_3d.distance.kpc,
+                s = 5, vmin = 1.5, vmax = 2.5, cmap = 'twilight')
+cb = fig.colorbar(cs)
+cb.set_label(f'distance [{u.kpc:latex_inline}]')
+
+ax.set_xlabel('RA [deg]')
+ax.set_ylabel('Dec [deg]')
+
+ax.set_title('Gaia DR2 sources near NGC 188', fontsize = 18)
+
+
+sep3d = ngc188_coords_3d.separation_3d(ngc188_center_3d)
+print(sep3d)
+
+
+
+# exercis2
+ngc188_3d_mask = sep3d < 50 * u.pc
+print(ngc188_3d_mask.sum())
+
+
+
+
+
+
+
+
